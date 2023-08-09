4dbbec4dce0b00343d8c643c4156233f631162ae
Sandeep Vishnu <sandeepkv93@gmail.com>
2023-08-08 17:31:52 -0700

    Add README.md

--- evenoddprinter/evenoddprinter.go
+++ evenoddprinter/evenoddprinter.go
@@ -6,11 +6,9 @@ import (
 )
 
 func main() {
-	// Create a Mutex for even and odd
 	var evenMutex sync.Mutex
 	var oddMutex sync.Mutex
 
-	// Create a WaitGroup to wait for all the go routines to finish
 	var wg sync.WaitGroup
 	wg.Add(2)
 	evenMutex.Lock()
